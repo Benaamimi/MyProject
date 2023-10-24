70534a05448cc0ebfe9b6707aaa6d37b9d532049
Tarik Benaamimi <benaamimit@gmail.com>
2023-10-24 20:52:15 +0200

    ajout de la page de detail d'un blogpost

--- src/Controller/BlogpostController.php
+++ src/Controller/BlogpostController.php
@@ -14,17 +14,29 @@ class BlogpostController extends AbstractController
     #[Route('/actualites', name: 'actualites')]
     public function actualites(BlogpostRepository $blogpostRepository, PaginatorInterface $paginator, Request $request): Response
     {
-
+        
         $data = $blogpostRepository->findAll(); // recupérer tous les articles
-
+        
         $blogposts = $paginator->paginate(
             $data,
             $request->query->getInt('page', 1), 6 // dans la requette http la valeur de 'page' par default = 1, limite de 6 articles par page 
         );
-
-
+        
+        
         return $this->render('blogpost/actualites.html.twig', [
             'blogposts' => $blogposts
+        ]);
+    }
+    
+    #[Route('/actualites/{slug}', name: 'actualites_detail')]
+    public function detail($slug, BlogpostRepository $blogpostRepository): Response
+    {
+        $blogpost = $blogpostRepository->findOneBy([
+            'slug' => $slug
+        ]);
+
+        return $this->render('blogpost/detail.html.twig',[
+            'blogpost' => $blogpost
         ]);
     }
 }
