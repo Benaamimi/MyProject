9b15d30866d6dda52f44a15fb23fb7071cad805d
Tarik Benaamimi <benaamimit@gmail.com>
2023-10-28 16:37:44 +0200

    gestion des parametre page a propos

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,7 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -12,14 +19,33 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('nom', 'Nom de famille'),
+            TextField::new('prenom', 'Prénom'),
+            // TextField::new('email', 'E-mail'),
+            TextField::new('telephone', 'Téléphone'),
+            TextareaField::new('aPropos', 'A propos'),
+            // TextField::new('password', 'Mot de passe')->setFormType(PasswordType::class)->onlyWhenCreating(),
+            // DateTimeField::new('dateEnregistrement', "Date d'enregistrement")->hideOnForm(),
+
         ];
     }
-    */
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->disable(Action::DELETE, Action::NEW);
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setPageTitle('index', 'Paramètres')
+            ->setPageTitle('edit', 'Editer les paramètres');
+    }
+    
 }
